1596cdd518e23e703fcec8c77d381d6085cfc58e
atri.alfa <atri.alfa@maucash.id>
2024-02-03 20:24:49 +0700

    initialize

---
+++ src/main/java/com/atri/puscerdas/PuscerdasApplication.java
@@ -0,0 +1,13 @@
+package com.atri.puscerdas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuscerdasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuscerdasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atri/puscerdas/PuscerdasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atri.puscerdas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuscerdasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
